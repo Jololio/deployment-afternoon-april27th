6fb4228f8836320986a6525ef18b7c70aeeb0c11
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-04-27 14:44:17 -0600

    added files I need

--- server/index.js
+++ server/index.js
@@ -7,11 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../index.html'))
-// })
-
-// app.use('/js', express.static(path.join(__dirname, 'server/index.js')))
+app.use('/js', express.static(path.join(__dirname, '../main.js')))
 
 const port = process.env.PORT || 4005
 
